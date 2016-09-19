4c090c0ddb9ed428128e9f3b438cf4b819fc2714
rjs <rjs@rjs-PC>
2016-09-19 20:25:04 +0800

    删除

---
+++ CZSJK/src/java/dvdrental/Cjhlei.java
@@ -0,0 +1,91 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dvdrental;
+
+/**
+ *
+ * @author rjs
+ */
+public class Cjhlei {
+    private String xingming;
+    private String xuehao;
+    private Integer nianling;
+    private String dianhua;
+    private String youxiang;
+
+   public Cjhlei(){
+   }
+
+    /**
+     * @return the xingming
+     */
+    public String getXingming() {
+        return xingming;
+    }
+
+    /**
+     * @param xingming the xingming to set
+     */
+    public void setXingming(String xingming) {
+        this.xingming = xingming;
+    }
+
+    /**
+     * @return the xuehao
+     */
+    public String getXuehao() {
+        return xuehao;
+    }
+
+    /**
+     * @param xuehao the xuehao to set
+     */
+    public void setXuehao(String xuehao) {
+        this.xuehao = xuehao;
+    }
+
+    /**
+     * @return the nianling
+     */
+    public Integer getNianling() {
+        return nianling;
+    }
+
+    /**
+     * @param nianling the nianling to set
+     */
+    public void setNianling(Integer nianling) {
+        this.nianling = nianling;
+    }
+
+    /**
+     * @return the dianhua
+     */
+    public String getDianhua() {
+        return dianhua;
+    }
+
+    /**
+     * @param dianhua the dianhua to set
+     */
+    public void setDianhua(String dianhua) {
+        this.dianhua = dianhua;
+    }
+
+    /**
+     * @return the youxiang
+     */
+    public String getYouxiang() {
+        return youxiang;
+    }
+
+    /**
+     * @param youxiang the youxiang to set
+     */
+    public void setYouxiang(String youxiang) {
+        this.youxiang = youxiang;
+    }
+}
